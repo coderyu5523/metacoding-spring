34caef631a12eaeec0ac12ec10055b44bba6f279
coderyu5523 <coderyu5523@naver.com>
2025-08-23 13:48:48 +0900

    게시글삭제 리팩토링

--- chap02/springv1/src/main/java/com/metacoding/springv1/board/BoardService.java
+++ chap02/springv1/src/main/java/com/metacoding/springv1/board/BoardService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
-
+import com.metacoding.springv1.config.Exception403;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import com.metacoding.springv1.config.Exception404; 
@@ -39,8 +39,12 @@ public class BoardService {
     }
 
     @Transactional
-    public void 게시글삭제(Integer id){
-        boardRepository.deleteById(id);
+    public void 게시글삭제(Integer boardId, Integer sessionUserId){
+        Board board = boardRepository.findByIdJoinUser(boardId).orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다."));
+        if(board.getUser().getId() != sessionUserId){
+            throw new Exception403("게시글을 삭제할 권한이 없습니다.");
+        }
+        boardRepository.deleteById(boardId);
     }
 
     public BoardResponse.DTO 게시글수정폼(Integer id){
---
+++ chap02/springv1/src/main/java/com/metacoding/springv1/config/Exception403.java
@@ -0,0 +1,7 @@
+package com.metacoding.springv1.config;
+
+public class Exception403 extends RuntimeException {
+    public Exception403(String msg) {
+        super(msg);
+    }
+}
